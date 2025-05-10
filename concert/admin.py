1468be172e80b8bce90c4e898490089ce4b9490a
Stergianni <stella.orfanidis@hotmail.com>
2025-05-10 17:31:26 +0200

    Update admin.py

--- concert/admin.py
+++ concert/admin.py
@@ -1,5 +1,12 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-# Register your models here.
+# # Register your models here.
+# admin.site.register(Concert)
+# from .models import Concert
+from django.contrib import admin
+from .models import Concert, Photo, Song, ConcertAttending  # <-- This line was missing
+
 admin.site.register(Concert)
-from .models import Concert
+admin.site.register(Photo)
+admin.site.register(Song)
+admin.site.register(ConcertAttending)
